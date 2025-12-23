6cbcda691a4297fad70c3485f826c321741b95c1
MaaxRasheed <243515@students.au.edu.pk>
2025-12-24 00:09:06 +0500

    Add files via upload

---
+++ Lab 14 BookStoreAPI/BookStoreAPI/Controllers/BooksController.cs
@@ -0,0 +1,61 @@
+﻿using BookStoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : Controller
+    {
+
+
+        private static List<Book> books = new List<Book>
+    {
+        new Book { BookId = 1, BookTitle = "C# Basics", Author = "Ali" },
+        new Book { BookId = 2, BookTitle = "ASP.NET Core", Author = "Ahmed" }
+    };
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(books);
+        }
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var book = books.FirstOrDefault(b => b.BookId == id);
+            return Ok(book);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {           
+            books.Add(book);
+            return Ok(books);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Book updatedBook)
+        {
+            var book = books.FirstOrDefault(b => b.BookId == id);
+            book.BookTitle = updatedBook.BookTitle;
+            book.Author = updatedBook.Author;
+            return Ok(book);
+        }
+
+     
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(b => b.BookId == id);
+            books.Remove(book);
+            return Ok(books);
+        }
+    }
+
+}
+
---
+++ Lab 14 BookStoreAPI/BookStoreAPI/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStoreAPI.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string?BookTitle { get; set; }
+        public string?Author { get; set; }
+
+    }
+}
